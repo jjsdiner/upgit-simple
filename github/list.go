126e32e1fd02f62138a0783dd83bf9d9122595fe
Richard Pogson <r.pogson@quidco.com>
2018-11-29 09:41:43 +0000

    Adding list functions

---
+++ github/list.go
@@ -0,0 +1,32 @@
+package github
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+//ListIssues queries the GitHub issue tracker and returns all issue on our repo.
+func ListIssues(terms []string) (*IssuesListResult, error) {
+	q := url.QueryEscape(strings.Join(terms, " "))
+	resp, err := http.Get(IssuesListURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+
+	//We must close resp.Body on all execution paths.
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("search query failed: %s", resp.Status)
+	}
+
+	var result IssuesListResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
+}
