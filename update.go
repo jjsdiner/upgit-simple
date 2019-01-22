0b4a87e89d82a7b1a303dcdb3fb3830de6a2e131
Richard Pogson <r.pogson@quidco.com>
2019-01-22 14:00:11 +0000

    work on the update method

--- update.go
+++ update.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"book/ch4/upgit/github"
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"strings"
 )
 
@@ -16,7 +20,25 @@ func validateUpdateType (userUpdateType string) (validatedUpdateType string, err
 	}
 }
 
-//Attempts to update the passed issue id, will open an editor to 
-func updateIssue (issueNumber int, updateType string){
+//Attempts to update the passed issue id, will open an editor if the description is being edited
+func updateIssue (issueNumber string, updateType string){
+
+	res, err := github.GetIssueDetails(issueNumber)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	selectedIssue := *res
+	switch updateType {
+	case	"state":
+	    fmt.Printf("Issue %s current status: %s enter open | closed to set state.\n", issueNumber, selectedIssue.State)
+        reader := bufio.NewReader(os.Stdin)
+        status, _ := reader.ReadString('\n')
+        fmt.Println(status)
+	default:
+		//Should not be possible but crash out
+		log.Fatalf("Invalid update type reached update command.")
+		os.Exit(1)
+	}
 
 }
